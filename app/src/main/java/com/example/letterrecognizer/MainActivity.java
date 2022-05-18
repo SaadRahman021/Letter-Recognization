8e0162b44fa6659395dfd6ffc82ce8e5ea702e28
Saad Rahman <bsef19m021@pucit.edu.pk>
2022-05-18 21:40:08 +0500

    Letter Recognizer Android App

---
+++ app/src/main/java/com/example/letterrecognizer/MainActivity.java
@@ -0,0 +1,206 @@
+package com.example.letterrecognizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        a = findViewById(R.id.button8);
+        b = findViewById(R.id.button4);
+        c=findViewById(R.id.button3);
+        d=findViewById(R.id.button5);
+        e=findViewById(R.id.button33);
+        f=findViewById(R.id.button7);
+        g=findViewById(R.id.button9);
+        h=findViewById(R.id.button13);
+        i=findViewById(R.id.button16);
+        j=findViewById(R.id.button19);
+        k=findViewById(R.id.button24);
+        l=findViewById(R.id.button27);
+        m=findViewById(R.id.button10);
+        n=findViewById(R.id.button14);
+        o=findViewById(R.id.button17);
+        p=findViewById(R.id.button20);
+        q=findViewById(R.id.button25);
+        r=findViewById(R.id.button28);
+        s=findViewById(R.id.button11);
+        t=findViewById(R.id.button15);
+        u=findViewById(R.id.button18);
+        v=findViewById(R.id.button21);
+        w=findViewById(R.id.button26);
+        x=findViewById(R.id.button29);
+        y=findViewById(R.id.button30);
+        z=findViewById(R.id.button31);
+
+        text.findViewById(R.id.textView);
+        a.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        c.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        e.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        i.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        m.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        n.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        o.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        r.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        s.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        u.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        v.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        w.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        x.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        z.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Grass Letter");
+            }
+        });
+        b.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        d.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        f.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        h.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        k.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        l.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        t.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Sky Letter");
+            }
+        });
+        g.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Root Letter");
+            }
+        });
+        j.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Root Letter");
+            }
+        });
+        p.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Root Letter");
+            }
+        });
+        q.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Root Letter");
+            }
+        });
+        y.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Root Letter");
+            }
+        });
+        }
+
+}
